c092f6544b4d70be16ede441f7d804d46aaf7e52
Mariana Alves <marianasdev@gmail.com>
2025-10-14 23:16:40 +0000

    feat: início das operações da calculadora

---
+++ pacote/Calculo.java
@@ -0,0 +1,15 @@
+package pacote;
+
+public class Calculo {
+    public static double somar(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtrair(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+}
---
+++ pacote/Main.java
@@ -0,0 +1,44 @@
+package pacote;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== CALCULADORA ===");
+        System.out.println("1 - Somar");
+        System.out.println("2 - Subtrair");
+        System.out.println("3 - Multiplicar");
+        System.out.println("4 - Dividir");
+        System.out.println("5 - Dobrar");
+        System.out.println("6 - Exponenciar");
+        System.out.println("7 - Sair");
+        System.out.print("\nEscolha uma opção: ");
+
+        String opcao = sc.nextLine();
+        System.out.println("");
+
+        System.out.print("Digite o 1° número: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Digite o 2° número: ");
+        double b = sc.nextDouble();
+
+        switch(opcao) {
+            case "1":
+                double resultadoSoma = Calculo.somar(a, b);
+                System.out.printf("O resultado da soma de %.2f com %.2f é %.2f.", a, b, resultadoSoma);
+                break;
+            case "2":
+                double resultadoSubtracao = Calculo.subtrair(a, b);
+                System.out.printf("O resultado da soma de %.2f com %.2f é %.2f.", a, b, resultadoSubtracao);
+                break;
+            case "3":
+                double resultadoProduto = Calculo.multiplicar(a, b);
+                System.out.printf("O resultado da soma de %.2f com %.2f é %.2f.", a, b, resultadoProduto);
+                break;
+        }
+
+        sc.close();
+    }
+}
