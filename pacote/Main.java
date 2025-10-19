cceff59465aff514c9d118422725a8a71c82f3c0
Mariana Alves <marianasdev@gmail.com>
2025-10-19 02:10:22 +0000

    fix: lógica finalizada e programa completo

--- pacote/Main.java
+++ pacote/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        double a = 0;
+        double b = 0;
+
         System.out.println("=== CALCULADORA ===\n"+
         "1 - Somar\n" +
         "2 - Subtrair\n" +
@@ -12,52 +15,53 @@ public class Main {
         "5 - Dobrar\n" +
         "6 - Exponenciar\n" +
         "7 - Sair\n" +
-        "\nEscolha uma opção: \n");      
+        "\nEscolha uma opção: ");      
 
         int opcao = sc.nextInt();
 
-        if (opcao <= 0 || opcao > 7) {
-            System.out.println("Opção inválida!");
-        } else {
-            System.out.print("Digite o 1° número: ");
-            double a = sc.nextDouble();
+        while (opcao <= 0 || opcao > 7) {
+            System.out.println("Opção inválida! Digite novamente: ");
+            opcao = sc.nextInt();
+        }
+
+        if (opcao != 5 && opcao != 7) {
+            System.out.print("\nDigite o 1° número: ");
+            a = sc.nextDouble();
 
             System.out.print("Digite o 2° número: ");
-            double b = sc.nextDouble();
+            b = sc.nextDouble();
 
-            switch(opcao) {
-                case 1:
-                    double resultadoSoma = Calculo.somar(a, b);
-                    System.out.printf("O resultado da soma de %.2f com %.2f é %.2f.", a, b, resultadoSoma);
-                    break;
-                case 2:
-                    double resultadoSubtracao = Calculo.subtrair(a, b);
-                    System.out.printf("O resultado da soma de %.2f com %.2f é %.2f.", a, b, resultadoSubtracao);
-                    break;
-                case 3:
-                    double resultadoProduto = Calculo.multiplicar(a, b);
-                    System.out.printf("O resultado da soma de %.2f com %.2f é %.2f.", a, b, resultadoProduto);
-                    break;
-                case 4:
-                    if (b == 0) {
-                        System.out.println("O divisor deve ser diferente de 0 (zero).");
-                        break;
-                    } else {
-                        double resultadoDivisao = Calculo.dividir(a, b);
-                        System.out.printf("O resultado da divisão de %.2f por %.2f é %.2f.", a, b, resultadoDivisao);
-                        break;
-                    }
-                case 5:
-                    double resultadoDobro = Calculo.dobrar(a);
-                    System.out.printf("O resultado do dobro de %.2f é %.2f.", a, resultadoDobro);
-                    break;
-                case 6:
-                    double resultadoExponencial = Calculo.exponenciar(a, b);
-                    System.out.printf("O resultado da soma de %.2f com %.2f é %.2f.", a, b, resultadoExponencial);
-                    break;
-                default:
-                    System.out.println("Inválido!");
+            while (opcao == 4 && b == 0) {
+                System.out.println("\nO divisor deve ser diferente de 0 (zero). Digite novamente o valor do denominador: ");
+                b = sc.nextInt();
             }
+        } else if (opcao == 5) {
+            System.out.print("\nDigite o número: ");
+            a = sc.nextDouble();
+        } else {
+            System.out.println("Encerrando o programa Calculadora.");
+            System.exit(0);
+        }
+
+        switch(opcao) {
+            case 1:
+                System.out.printf("\nO resultado da soma de %.2f com %.2f é %.2f.", a, b, Calculo.somar(a, b));
+                break;
+            case 2:
+                System.out.printf("\nO resultado da subtração de %.2f com %.2f é %.2f.", a, b, Calculo.subtrair(a, b));
+                break;
+            case 3:
+                System.out.printf("\nO resultado da multiplicação de %.2f por %.2f é %.2f.", a, b, Calculo.multiplicar(a, b));
+                break;
+            case 4:
+                System.out.printf("\nO resultado da divisão de %.2f por %.2f é %.2f.", a, b, Calculo.dividir(a, b));
+                break;
+            case 5:
+                System.out.printf("\nO dobro de %.2f é %.2f.", a, Calculo.dobrar(a));
+                break;
+            case 6:
+                System.out.printf("\nO resultado da base %.2f pelo expoente %.2f é %.2f.", a, b, Calculo.exponenciar(a, b));
+                break;
         }
         
         sc.close();
